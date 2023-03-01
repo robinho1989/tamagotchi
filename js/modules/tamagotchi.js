3fab08e2937062b62fac459549cf460c87951cbd
Robert <robinho1989@wp.pl>
2023-03-01 21:47:17 +0000

    life params implemented-problem wit healthDecreaseh

--- js/modules/tamagotchi.js
+++ js/modules/tamagotchi.js
@@ -5,21 +5,52 @@ export default class Tamagotchi {
 		this.energy = { value: 10, importance: 2 };
 		this.fun = { value: 10, importance: 4 };
 		console.log('Tamagotchi initialized');
-		this.healthDecrease = setInterval(() => {
-			this.health.value -= 1;
-			if (this.health.value <= 0) {
-				clearInterval(this.healthDecrease);
-			}
-			// this.displayHealth();
-			
-		}, 1000);
+
+		const energyValue = '.energy';
+		const hungerValue = '.hunger';
+		const funValue = '.fun';
+		const healthValue = '.health';
+
 		this.energyDecrease = setInterval(() => {
-			this.energy.value -= 1;
+			let newDecrementValue = 2;
+
+			if (this.fun.value <= 0) {
+				this.energy.value -= newDecrementValue;
+				if (this.energy.value <= 0) {
+					this.energy.value = 0;
+				}
+			} else {
+				this.energy.value -= 1;
+			}
 			if (this.energy.value <= 0) {
 				clearInterval(this.energyDecrease);
 			}
-			this.displayEnergy()
+			this.displayEnergy(energyValue);
 		}, 2000);
+		this.hungerDecrease = setInterval(() => {
+			this.hunger.value -= 1;
+			if (this.hunger.value <= 0) {
+				clearInterval(this.hungerDecrease);
+			}
+			this.displayHunger(hungerValue);
+		}, 1000);
+		this.funDecrease = setInterval(() => {
+			this.fun.value -= 1;
+			if (this.fun.value <= 0) {
+				clearInterval(this.funDecrease);
+			}
+			this.displayFun(funValue);
+		}, 1000);
+		this.healthDecrease = setInterval(() => {
+			if (this.energy.value <= 0 || this.hunger.value <= 0) {
+				this.health.value -= 1;
+				console.log(this.health.value);
+				this.displayHealth(healthValue);
+				if (this.health.value <= 0) {
+					clearInterval(this.healthDecrease);
+				}
+			}
+		}, 1000);
 	}
 
 	displayHealth = (elementSelector) => {
