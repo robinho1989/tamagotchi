729726b2a44c1711a9dd8d273dffb583c152ded3
Robert <robinho1989@wp.pl>
2023-03-04 18:38:48 +0000

    healthDecrease method fixed

--- js/modules/tamagotchi.js
+++ js/modules/tamagotchi.js
@@ -5,49 +5,15 @@ export default class Tamagotchi {
 		this.energy = { value: 10, importance: 2, element: null };
 		this.fun = { value: 10, importance: 4, element: null };
 		console.log('Tamagotchi initialized');
-		this.energyDecrease = setInterval(() => {
-			let newDecrementValue = 2;
-			if (this.fun.value <= 0) {
-				this.energy.value -= newDecrementValue;
-				if (this.energy.value <= 0) {
-					this.energy.value = 0;
-				}
-			} else {
-				this.energy.value -= 1;
-			}
-			if (this.energy.value <= 0) {
-				clearInterval(this.energyDecrease);
-			}
-			this.displayEnergy(this.energy.element);
-		}, 2000);
-		this.hungerDecrease = setInterval(() => {
-			this.hunger.value -= 1;
-			if (this.hunger.value <= 0) {
-				clearInterval(this.hungerDecrease);
-			}
-			this.displayHunger(this.hunger.element);
-		}, 1000);
-		this.funDecrease = setInterval(() => {
-			this.fun.value -= 1;
-			if (this.fun.value <= 0) {
-				clearInterval(this.funDecrease);
-			}
-			this.displayFun(this.fun.element);
-		}, 1000);
-		this.healthDecrease = setInterval(() => {
-			if (this.energy.value <= 0 || this.hunger.value <= 0) {
-				this.health.value -= 1;
-				this.displayHealth(this.health.element);
-				if (this.health.value <= 0) {
-					clearInterval(this.healthDecrease);
-				}
-			}
-		}, 1000);
+		this.healthDecrease();
+		this.energyDecrease();
+		this.hungerDecrease();
+		this.funDecrease();
 	}
 
 	displayHealth = (elementSelector) => {
 		const displayElement = document.querySelector(elementSelector);
-		displayElement.innerText = this.energy.value;
+		displayElement.innerText = this.health.value;
 	};
 	displayHunger = (elementSelector) => {
 		const displayElement = document.querySelector(elementSelector);
@@ -61,7 +27,52 @@ export default class Tamagotchi {
 		const displayElement = document.querySelector(elementSelector);
 		displayElement.innerText = this.fun.value;
 	};
-
+	energyDecrease = () => {
+		const decreaseValue = setInterval(() => {
+			let newDecrementValue = 2;
+			if (this.fun.value <= 0) {
+				this.energy.value -= newDecrementValue;
+				if (this.energy.value <= 0) {
+					this.energy.value = 0;
+				}
+			} else {
+				this.energy.value -= 1;
+			}
+			if (this.energy.value <= 0) {
+				clearInterval(decreaseValue);
+			}
+			this.displayEnergy(this.energy.element);
+		}, 2000);
+	};
+	hungerDecrease = () => {
+		const decreaseValue = setInterval(() => {
+			this.hunger.value -= 1;
+			if (this.hunger.value <= 0) {
+				clearInterval(decreaseValue);
+			}
+			this.displayHunger(this.hunger.element);
+		}, 1000);
+	};
+	funDecrease = () => {
+		const decreaseValue = setInterval(() => {
+			this.fun.value -= 1;
+			if (this.fun.value <= 0) {
+				clearInterval(decreaseValue);
+			}
+			this.displayFun(this.fun.element);
+		}, 1000);
+	};
+	healthDecrease = () => {
+		const decreaseValue = setInterval(() => {
+			if (this.energy.value <= 0 || this.hunger.value <= 0) {
+				this.health.value -= 1;
+				this.displayHealth(this.health.element);
+				if (this.health.value <= 0) {
+					clearInterval(decreaseValue);
+				}
+			}
+		}, 1000);
+	};
 	mount = ({ healthElement, hungerElement, energyElement, funElement }) => {
 		this.health.element = healthElement;
 		this.hunger.element = hungerElement;
